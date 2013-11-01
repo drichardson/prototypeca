a6e5e40c848ec6361b0e2039f8032968e80cbea1
Doug Richardson <dougie.richardson@gmail.com>
2013-11-01 08:59:28 -0700

    WIP: Trying to make my non-document based application a document based one.

---
+++ PrototypeCA/PrototypeCA/PCADocument.h
@@ -0,0 +1,13 @@
+//
+//  PCADocument.h
+//  PrototypeCA
+//
+//  Created by Douglas Richardson on 10/30/13.
+//  Copyright (c) 2013 PrototypeCA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PCADocument : NSDocument
+
+@end
